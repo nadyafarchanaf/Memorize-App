618cf23cb4b136c69dd62e6218bb39320b6f6523
Nadya Farchana Fidaroina <nadyafarchanaf@gmail.com>
2021-08-05 16:49:11 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Asani on 05/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
